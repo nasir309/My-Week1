8d9753c122c2678eac254166d04c475bf0e441b2
nasir <nasiradan916@gmail.com>
2025-06-16 09:24:40 +0300

    add

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Gallery from './Components/Gallery'
+
+
+
+
+function App() {
+  return (
+    <>
+    <Gallery></Gallery>
+    
+    </>
+  )
+}
+
+export default App
